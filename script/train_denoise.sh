f06683df482320ee5dd28f88226df2b4d45ba54d
cuonglannguyen <94313142+cuonglannguyen@users.noreply.github.com>
2023-07-09 00:19:23 +0700

    Update train_denoise.sh

--- script/train_denoise.sh
+++ script/train_denoise.sh
@@ -1,4 +1,4 @@
-python3 ./train/train_denoise.py --arch Uformer_T --batch_size 16 --gpu '0' \
+python3 ./train/train_denoise.py --arch Uformer_T --batch_size 32 --gpu '0' \
     --train_ps 128 --train_dir ../datasets/denoising/sidd/train --env _0706 \
     --val_dir /kaggle/input/sidd-val-patches/validation-sidd --save_dir ./logs/ \
     --dataset sidd --warmup 
