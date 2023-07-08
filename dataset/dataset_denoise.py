f71d8ed15557888863bd65c84328fb1578e94da3
cuonglannguyen <94313142+cuonglannguyen@users.noreply.github.com>
2023-07-08 22:53:19 +0700

    Update dataset_denoise.py

--- dataset/dataset_denoise.py
+++ dataset/dataset_denoise.py
@@ -24,7 +24,7 @@ class DataLoaderTrain(Dataset):
         self.target_transform = target_transform
         
         gt_dir = 'groundtruth' 
-        input_dir = 'input'
+        input_dir = 'noisy'
         
         clean_files = sorted(os.listdir(os.path.join(rgb_dir, gt_dir)))
         noisy_files = sorted(os.listdir(os.path.join(rgb_dir, input_dir)))
