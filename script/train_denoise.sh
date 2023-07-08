c11071d9c74dc5beb294460a2a3f0e63390bc69b
cuonglannguyen <94313142+cuonglannguyen@users.noreply.github.com>
2023-07-08 22:24:52 +0700

    Update train_denoise.sh

--- script/train_denoise.sh
+++ script/train_denoise.sh
@@ -1,4 +1,4 @@
-python3 ./train/train_denoise.py --arch Uformer_B --batch_size 32 --gpu '0,1' \
+python3 ./train/train_denoise.py --arch Uformer_B --batch_size 32 --gpu '0' \
     --train_ps 128 --train_dir ../datasets/denoising/sidd/train --env _0706 \
     --val_dir ../datasets/denoising/sidd/val --save_dir ./logs/ \
     --dataset sidd --warmup 
